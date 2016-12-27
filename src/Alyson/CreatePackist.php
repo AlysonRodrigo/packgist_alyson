1e8c33015a9df3ad166c258c4823092314d718d2
Alyson Rodrigo <g.dasilva@hotmail.com>
2016-12-27 18:44:56 -0300

    Commit inicial

---
+++ src/Alyson/CreatePackist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alyson
+ * Date: 27/12/16
+ * Time: 17:51
+ */
+
+namespace Alyson\App;
+
+class CreatePackist
+{
+    public function olaMundo(){
+        return "Ola mundo!";
+    }
+}
